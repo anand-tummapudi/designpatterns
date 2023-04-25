09a126fea75591894af5f638bdaaf65e5cdafc53
anand tummapudi <anand.tummapudi@gmail.com>
2023-04-25 13:41:59 +0530

    Updated Singleton showing breaking using reflection

--- src/com/anand/creational/singleton/SingletonImpl.java
+++ src/com/anand/creational/singleton/SingletonImpl.java
@@ -1,5 +1,7 @@
 package com.anand.creational.singleton;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -13,28 +15,46 @@ public class SingletonImpl {
 		 * 
 		 * System.out.println(obj1.hashCode()); System.out.println(obj2.hashCode());
 		 */
+		/*
+		 * ExecutorService executorService = null; MyThread myThread = new MyThread();
+		 * 
+		 * try{ executorService = Executors.newFixedThreadPool(5);
+		 * executorService.execute(myThread); executorService.execute(myThread);
+		 * executorService.execute(myThread); executorService.execute(myThread);
+		 * executorService.execute(myThread); executorService.execute(myThread);
+		 * executorService.execute(myThread); executorService.execute(myThread);
+		 * executorService.execute(myThread);
+		 * 
+		 * } catch(Exception e) { e.printStackTrace(); }finally {
+		 * 
+		 * }
+		 */
 		
-		ExecutorService executorService = null;
-		MyThread myThread = new MyThread();
+		// Using Reflection API to break singleton
 		
-		try{
-			executorService = Executors.newFixedThreadPool(5);
-			executorService.execute(myThread);
-			executorService.execute(myThread);
-			executorService.execute(myThread);
-			executorService.execute(myThread);
-			executorService.execute(myThread);
-			executorService.execute(myThread);
-			executorService.execute(myThread);
-			executorService.execute(myThread);
-			executorService.execute(myThread);
-			
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}finally {
-			
-		}
+		 Singleton obj1 = Singleton.getInstance(); 
+		 Singleton obj2 = null;
+		 
+		 Constructor<?>[] constructors = Singleton.class.getDeclaredConstructors();
+		 
+		 for(Constructor<?> constructor:constructors) {
+			 constructor.setAccessible(true);
+			 
+			 try {
+				Object object = constructor.newInstance();
+				obj2 = (Singleton)object;
+				break;
+				
+			} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+					| InvocationTargetException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		 }
+		 
+		 System.out.println(obj1.hashCode());
+		 System.out.println(obj2.hashCode());
+		
 		
 	}
 
